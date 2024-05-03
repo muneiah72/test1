36e774045b7af481e73506423562eb29c4c6b39d
Muneiah <muneiah.gade@gmail.com>
2024-05-03 08:56:43 +0000

    Changes to custom event

--- dist/index.js
+++ dist/index.js
@@ -31085,15 +31085,11 @@ try {
   // `title` input defined in action metadata file
   const title = core.getInput('title', { required: true });
   console.log(`Hello ${title}!`);
-  const reviewer = core.getInput('reviewer', { required: true });
-  console.log(`Hello ${reviewer}!`);
-  const pat = core.getInput('pat', { required: true });
-  console.log(`Hello ${pat}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload)
-  const data = JSON.stringify(github.context.payload.inputs)
+  const data = JSON.parse(JSON.stringify(github.context.payload.inputs))
   console.log(`The payload title: ${data.title}`);
   console.log(`The event payload: ${payload}`);
 
